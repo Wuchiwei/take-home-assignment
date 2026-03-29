bd5b148af1e3fc59454c5c0b5d78aae6a6af32c9
Luke.Wu <wuchiwei17@gmail.com>
2026-03-29 13:01:56 +0800

    Initial Commit

---
+++ OpenNet/OpenNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenNetApp.swift
+//  OpenNet
+//
+//  Created by LukeWu on 2026/3/28.
+//
+
+import SwiftUI
+
+@main
+struct OpenNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenNetTests/OpenNetTests.swift
@@ -0,0 +1,16 @@
+//
+//  OpenNetTests.swift
+//  OpenNetTests
+//
+//  Created by LukeWu on 2026/3/28.
+//
+
+import Testing
+
+struct OpenNetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
